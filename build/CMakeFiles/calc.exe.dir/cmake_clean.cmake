71ba88632b6717baab63d12786a47d100855ca47
Irina <sekti2004@yandex.ru>
2023-12-19 19:59:46 +0300

    Add leb

---
+++ build/CMakeFiles/calc.exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calc.exe.dir/test/server.cpp.o"
+  "CMakeFiles/calc.exe.dir/test/server.cpp.o.d"
+  "calc.exe"
+  "calc.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calc.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/control.exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/control.exe.dir/test/client.cpp.o"
+  "CMakeFiles/control.exe.dir/test/client.cpp.o.d"
+  "control.exe"
+  "control.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/control.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
